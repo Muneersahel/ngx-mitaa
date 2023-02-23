36005c804acb189df06f537ba68d2f59d4074596
Muneersahel <saidmunir73@gmail.com>
2023-02-23 23:21:08 +0300

    add build functionality in github workflow

--- dist/ngx-mitaa/lib/utils/locations/africa/tanzania/kilimanjaro/kilimanjaro.d.ts
+++
@@ -1,2 +0,0 @@
-import { Region } from '../../../../ngx-mitaa.interfaces';
-export declare const kilimanjaro: Region;
--- dist/ngx-mitaa/public-api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/ngx-mitaa.service';
-export * from './lib/utils/ngx-mitaa.interfaces';
-export * from './lib/utils/ngx-mitaa.locations';
