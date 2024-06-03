ae70363683406492c7c7a8c64e100e3696ad610b
Justini  Lasway <jastinlasway10@gmail.com>
2024-06-03 10:22:19 +0300

    adding nairobi region and its related information i.e ward, street and discrict

---
+++ dist/ngx-mitaa/lib/utils/locations/africa/kenya/nairobi/nairobi.ts
@@ -0,0 +1,164 @@
+import { Region } from '../../../../ngx-mitaa.interfaces';
+
+export const nairobi: Region = {
+  region: 'Nairobi',
+  districts: [
+    {
+      district: 'Central Business District',
+      wards: [
+        {
+          ward: 'Starehe',
+          streets: [
+            'Moi Avenue',
+            'Tom Mboya Street',
+            'Ronald Ngala Street',
+            'Haile Selassie Avenue',
+            'Kenyatta Avenue',
+          ],
+        },
+        {
+          ward: 'Kamukunji',
+          streets: ['Eastleigh', 'Pumwani', 'Gikomba'],
+        },
+        {
+          ward: 'Ngara',
+          streets: ['Ngara Road', 'Muranga Road', 'Park Road'],
+        },
+        {
+          ward: 'Pangani',
+          streets: ['Juja Road', 'Pangani Shopping Centre'],
+        },
+        {
+          ward: 'Muthurwa',
+          streets: ['Muthurwa Market', 'Landhies Road'],
+        },
+      ],
+    },
+    {
+      district: 'Westlands',
+      wards: [
+        {
+          ward: 'Parklands',
+          streets: ['Limuru Road', 'Ojijo Road'],
+        },
+        {
+          ward: 'Kilimani',
+          streets: ['Argwings Kodhek Road', 'Ngong Road'],
+        },
+        {
+          ward: 'Kangemi',
+          streets: ['Waiyaki Way', 'Kangemi Market'],
+        },
+        {
+          ward: 'Mountain View',
+          streets: ['Mountain View Estate', 'Kangemi'],
+        },
+      ],
+    },
+    {
+      district: 'Langata',
+      wards: [
+        {
+          ward: 'Karen',
+          streets: ['Karen Road', 'Ngong Road', 'Langata South Road'],
+        },
+        {
+          ward: 'Nairobi West',
+          streets: ['Langata Road', 'Mbagathi Way'],
+        },
+        {
+          ward: 'Mugumoini',
+          streets: ['Langata Road', 'Mbagathi Way'],
+        },
+        {
+          ward: 'South C',
+          streets: ['Mombasa Road', 'Bellevue'],
+        },
+      ],
+    },
+    {
+      district: 'Embakasi',
+      wards: [
+        {
+          ward: 'Embakasi',
+          streets: ['Outering Road', 'Airport North Road'],
+        },
+        {
+          ward: 'Umoja',
+          streets: ['Kangundo Road', 'Moi Drive'],
+        },
+        {
+          ward: 'Donholm',
+          streets: ['Donholm Road', 'Savanah'],
+        },
+        {
+          ward: 'Kayole',
+          streets: ['Kangundo Road', 'Spine Road'],
+        },
+      ],
+    },
+    {
+      district: 'Kasarani',
+      wards: [
+        {
+          ward: 'Roysambu',
+          streets: ['Thika Road', 'Lumumba Drive'],
+        },
+        {
+          ward: 'Kasarani',
+          streets: ['Mwiki Road', 'Kasarani Road'],
+        },
+        {
+          ward: 'Ruaraka',
+          streets: ['Outering Road', 'Thika Road'],
+        },
+        {
+          ward: 'Babadogo',
+          streets: ['Kariobangi North', 'Kariobangi South'],
+        },
+      ],
+    },
+    {
+      district: 'Dagoretti',
+      wards: [
+        {
+          ward: 'Kawangware',
+          streets: ['Kawangware 46', 'Kawangware 56'],
+        },
+        {
+          ward: 'Riruta',
+          streets: ['Ngong Road', 'Dagoretti Corner'],
+        },
+        {
+          ward: 'Uthiru',
+          streets: ['Kikuyu Road', 'Uthiru Shopping Centre'],
+        },
+        {
+          ward: 'Waithaka',
+          streets: ['Ngong Road', 'Waithaka Shopping Centre'],
+        },
+      ],
+    },
+    {
+      district: 'Makadara',
+      wards: [
+        {
+          ward: 'Makadara',
+          streets: ['Jogoo Road', 'Hamza'],
+        },
+        {
+          ward: 'Harambee',
+          streets: ['Jogoo Road', 'Burma Market'],
+        },
+        {
+          ward: 'Viwandani',
+          streets: ['Landhies Road', 'Industrial Area'],
+        },
+        {
+          ward: 'Maringo/Hamza',
+          streets: ['Jogoo Road', 'Maringo'],
+        },
+      ],
+    },
+  ],
+};
